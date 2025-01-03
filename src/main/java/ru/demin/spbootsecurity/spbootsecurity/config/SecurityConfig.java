b81d43417d1f8ed596e7c343989cff65f347a214
Alexandr Demin <deminas83@gmail.com>
2025-01-03 15:09:48 +0300

    изменил на стандартную аутентификацию

--- src/main/java/ru/demin/spbootsecurity/spbootsecurity/config/SecurityConfig.java
+++ src/main/java/ru/demin/spbootsecurity/spbootsecurity/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package ru.demin.spbootsecurity.spbootsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.demin.spbootsecurity.spbootsecurity.services.PersonDetailsService;
 
-import ru.demin.spbootsecurity.spbootsecurity.security.AuthProviderImpl;
-
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure (AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
