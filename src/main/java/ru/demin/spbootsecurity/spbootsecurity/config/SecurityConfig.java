82ffca3513edc8fb0bc2d386504d929b509df091
Alexandr Demin <deminas83@gmail.com>
2025-01-03 17:46:39 +0300

    добавил securityFilterChain

--- src/main/java/ru/demin/spbootsecurity/spbootsecurity/config/SecurityConfig.java
+++ src/main/java/ru/demin/spbootsecurity/spbootsecurity/config/SecurityConfig.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.demin.spbootsecurity.spbootsecurity.services.PersonDetailsService;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     private final PersonDetailsService personDetailsService;
@@ -20,8 +21,27 @@ public class SecurityConfig {
         this.personDetailsService = personDetailsService;
     }
 
-    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/auth/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.userDetailsService(personDetailsService);
     }
 
     @Bean
---
+++ src/main/java/ru/demin/spbootsecurity/spbootsecurity/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package ru.demin.spbootsecurity.spbootsecurity.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "auth/login";
+    }
+}
